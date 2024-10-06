3ef0a2a7808af49240aba3d9927c8497edd3658d
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-06 19:37:16 +0330

    Create front for my project

---
+++ src/Client/Mango.Client.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mango.Client.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
