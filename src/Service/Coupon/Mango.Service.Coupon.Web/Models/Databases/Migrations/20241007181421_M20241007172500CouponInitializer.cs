257e95c6791f7ac1ab8a1525e12119005bc79729
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-07 22:25:28 +0330

    Add migration of entity

---
+++ src/Service/Coupon/Mango.Service.Coupon.Web/Models/Databases/Migrations/20241007181421_M20241007172500CouponInitializer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Mango.Service.Coupon.Web.Models.Databases.Migrations
+{
+    /// <inheritdoc />
+    public partial class M20241007172500CouponInitializer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "coupons",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    code = table.Column<string>(type: "longtext", nullable: false),
+                    discount_amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    minimum_amount = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_coupons", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_coupons_id",
+                table: "coupons",
+                column: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "coupons");
+        }
+    }
+}
