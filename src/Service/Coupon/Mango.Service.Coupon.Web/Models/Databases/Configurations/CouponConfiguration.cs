0bccec90dde324033c136c3e8ced87a2103f1583
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-07 22:21:56 +0330

    Create data context, configuration and migration of it

---
+++ src/Service/Coupon/Mango.Service.Coupon.Web/Models/Databases/Configurations/CouponConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mango.Service.Coupon.Web.Models.Databases.Configurations;
+
+public sealed class CouponConfiguration : IEntityTypeConfiguration<Entities.Coupon>
+{
+    public void Configure(EntityTypeBuilder<Entities.Coupon> builder)
+    {
+        builder.HasIndex(model => model.Id);
+
+        builder.Property(model => model.Code)
+            .IsRequired();
+
+        builder.Property(model => model.DiscountAmount)
+            .IsRequired();
+    }
+}
---
+++ src/Service/Coupon/Mango.Service.Coupon.Web/Models/Databases/Contexts/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Service.Coupon.Web.Models.Databases.Contexts;
+
+public sealed class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
+{
+    public DbSet<Entities.Coupon> Coupons { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+    }
+}
