4fadf65f20934317cb1f3018c9dcdb65da120db1
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-07 22:20:30 +0330

    Create entities

---
+++ src/Service/Coupon/Mango.Service.Coupon.Web/Models/Entities/Coupon.cs
@@ -0,0 +1,27 @@
+﻿namespace Mango.Service.Coupon.Web.Models.Entities;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class Coupon
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Code { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public decimal DiscountAmount { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public decimal? MinimumAmount { get; set; }
+}
