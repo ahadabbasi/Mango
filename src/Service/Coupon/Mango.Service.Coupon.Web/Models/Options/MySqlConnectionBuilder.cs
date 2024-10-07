3e64d5b3e4470960bda5d8dd3f3b2b1925ebdbc7
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-07 22:22:25 +0330

    Create connection builder option

---
+++ src/Service/Coupon/Mango.Service.Coupon.Web/Models/Options/MySqlConnectionBuilder.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Mango.Service.Coupon.Web.Models.Options;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class MySqlConnectionBuilder
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public const string ApplicationSettingSectionName = "MySqlConnection";
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Server { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Port { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Database { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Username { get; set; } = string.Empty;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Password { get; set; } = string.Empty;
+
+    public string ConnectionString()
+    {
+        MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder()
+        {
+            Server = Server,
+            Port = uint.Parse(Port),
+            Database = Database,
+            UserID = Username,
+            Password = Password
+        };
+
+        return builder.ConnectionString;
+    }
+    
+}
