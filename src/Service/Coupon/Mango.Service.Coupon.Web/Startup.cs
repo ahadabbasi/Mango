4d6ceb8822e8054a39955be4d3ec1bc3f5305aa7
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-07 22:24:35 +0330

    Implement connection to setting and then added to dependencies to apply on entity framework

--- src/Service/Coupon/Mango.Service.Coupon.Web/Startup.cs
+++ src/Service/Coupon/Mango.Service.Coupon.Web/Startup.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Mango.Service.Coupon.Web.Models.Databases.Contexts;
+using Mango.Service.Coupon.Web.Models.Extensions;
+using Mango.Service.Coupon.Web.Models.Options;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace Mango.Service.Coupon.Web;
 
@@ -11,12 +17,30 @@ public static class Startup
     public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
         // Add services to the container.
-        
+        services.Configure<MySqlConnectionBuilder>(
+            configuration.GetSection(
+                MySqlConnectionBuilder.ApplicationSettingSectionName
+            )
+        );
+
+        services.AddDbContext<ApplicationContext>((provider, builder) =>
+        {
+            IOptions<MySqlConnectionBuilder>? connectionBuilder = provider.GetService<IOptions<MySqlConnectionBuilder>>();
+
+            if (connectionBuilder == null)
+            {
+                throw new Exception("Connection builder is null");
+            }
+
+            builder.UseMySQL(connectionBuilder.Value.ConnectionString())
+                .UseSnakeCaseNamingConvention();
+        });
+
         services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
-        
+
         services.AddRouting(options =>
         {
             options.LowercaseUrls = true;
@@ -24,10 +48,10 @@ public static class Startup
         });
     }
 
-    public static void Configure(WebApplication app, IWebHostEnvironment env)
+    public static async void Configure(WebApplication app, IWebHostEnvironment env)
     {
         // Configure the HTTP request pipeline.
-        
+
         if (env.IsDevelopment())
         {
             app.UseSwagger();
@@ -37,7 +61,11 @@ public static class Startup
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
-
+        
         app.MapControllers();
+
+        await app.UseMigrations(typeof(Startup).Assembly);
+
+        
     }
 }
