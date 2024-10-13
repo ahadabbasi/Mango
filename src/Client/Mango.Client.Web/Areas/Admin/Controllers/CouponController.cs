4deae74bb68a368a166f4e62eae4ce23c2e5e025
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-13 19:09:33 +0330

    Trying to add admin area with coupon controller

---
+++ src/Client/Mango.Client.Web/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Mango.Client.Web.Models.Configurations;
+using Mango.Client.Web.Models.Coupon.Contracts;
+using Mango.Client.Web.Models.Coupon.ViewModels;
+using Mango.Common.Shared.Result;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Client.Web.Areas.Admin.Controllers;
+
+[Area(AreaName.Admin)]
+public class CouponController(ICouponService service) : Controller
+{
+    // GET
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        return View(await service.AllAsync(cancellationToken));
+    }
+}
---
+++ src/Client/Mango.Client.Web/Areas/Admin/Models/Configurations/Views.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Client.Web.Areas.Admin.Models.Configurations;
+
+public sealed class Views
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public const string Title = nameof(Title);
+    
+    public const string Styles = nameof(Styles);
+    
+    public const string Scripts = nameof(Scripts);
+}
