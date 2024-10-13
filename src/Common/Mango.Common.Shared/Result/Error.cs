080424e52f243e734a536385a91582765d45fa23
Ahad Abbasi <ahadabbasi@gmail.com>
2024-10-13 18:41:48 +0330

    Add new error as server not response

--- src/Common/Mango.Common.Shared/Result/Error.cs
+++ src/Common/Mango.Common.Shared/Result/Error.cs
@@ -6,5 +6,7 @@ public record Error(string Code, string? Description = null)
     
     public static readonly Error NotFound = new Error(string.Empty);
     
+    public static readonly Error ServerNotResponse = new Error(string.Empty);
+    
     public static implicit operator Error(string code) => new Error(code);
 }
