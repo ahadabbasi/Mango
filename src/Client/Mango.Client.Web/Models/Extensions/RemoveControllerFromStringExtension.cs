529d9f4e14755ecbaa81a97e8103f9aa763e0b81
Ahad Abbasi <ahadabbasi@gmail.com>
2025-02-11 21:35:24 +0330

    Extension for removing controller name from the plain string

---
+++ src/Client/Mango.Client.Web/Models/Extensions/RemoveControllerFromStringExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Client.Web.Models.Extensions;
+
+public static class RemoveControllerFromStringExtension
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="entry"></param>
+    /// <returns></returns>
+    public static string RemoveControllerFromString(this string entry)
+    {
+        return entry.Replace(nameof(Controller), string.Empty);
+    }
+}
